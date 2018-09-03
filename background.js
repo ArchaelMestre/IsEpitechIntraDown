7eb3ebd016bce51c9f2f515b502acef74abb5ad0
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-09-03 17:53:19 +0200

    Add css color and one line link

--- background.js
+++ background.js
@@ -3,8 +3,8 @@ chrome.browserAction.setBadgeText({text: " " })
 function checkIntra() {
     var http = new XMLHttpRequest();
     http.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
                 chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
             } else {
                 chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
