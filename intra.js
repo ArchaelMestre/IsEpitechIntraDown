3548ed800def650d8d7ce5395a558735831b7197
Xwilarg <xwilarg@yahoo.fr>
2018-09-03 19:51:34 +0200

    Add request to intra API

---
+++ intra.js
@@ -0,0 +1,14 @@
+function GetMessage() {
+    let http = new XMLHttpRequest();
+    http.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                document.getElementById('message').innerHTML = "OK";
+            } else {
+                document.getElementById('message').innerHTML = "ERROR";
+            }
+        }
+    };
+    http.open("HEAD", "https://intra.epitech.eu/user/notification/message?format=json", true);
+    http.send();
+}
