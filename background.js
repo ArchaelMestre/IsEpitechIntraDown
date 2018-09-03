f1da6670adf8398a71d44178bfb97a2c3e7bc6f0
Xwilarg <xwilarg@yahoo.fr>
2018-09-03 17:24:41 +0200

    Use XMLHttpRequest

--- background.js
+++ background.js
@@ -1,13 +1,21 @@
 chrome.browserAction.setBadgeText({text: " " })
 
-setInterval(function () {
-    let img = new Image();
-    img.src = "https://intra.epitech.eu/static530ebd62bc289f486af254df31812fa9fdeaa0e7/img/logo.png";
-    img.onload = function () {
-        if (img.naturalWidth === 0) {
-            chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-        } else {
-            chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
+function checkIntra() {
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
+            } else {
+                chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+            }
         }
     };
+    http.open("HEAD", "https://intra.epitech.eu", true);
+    http.send();
+}
+
+checkIntra();
+setInterval(function () {
+   checkIntra();
 }, 3000);
