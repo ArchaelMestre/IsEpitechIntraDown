4662ecc9b7582816f3c622d96e9c5c2cba8b3af5
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2018-09-03 17:10:29 +0200

    Change badge color

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+chrome.browserAction.setBadgeText({text: " " })
+
 let button = document.getElementById("isUp");
 
 setInterval(function () {
     let img = new Image();
     img.src = "https://intra.epitech.eu/static530ebd62bc289f486af254df31812fa9fdeaa0e7/img/logo.png";
     img.onload = function () {
-        console.log("UP"); button.innerText = "UP";
-        if (img.naturalWidth === 0)
+        if (img.naturalWidth === 0) {
             button.innerText = "DOWN";
+            chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+        } else {
+            button.innerText = "UP";
+            chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
+        }
     };
 }, 3000);
