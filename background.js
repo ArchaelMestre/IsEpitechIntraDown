2b5622568cef123c91580bbf6bf1d5049402ffc3
Xwilarg <xwilarg@yahoo.fr>
2018-09-04 14:38:04 +0200

    Change badge color when internet isn't available

--- background.js
+++ background.js
@@ -1,14 +1,27 @@
 chrome.browserAction.setBadgeText({text: " " })
 
 function checkIntra() {
-    var http = new XMLHttpRequest();
+    let http = new XMLHttpRequest();
     http.timeout = 5000;
     http.onreadystatechange = function() {
         if (this.readyState === 4) {
             if (this.status === 200 || this.status === 401) {
                 chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
             } else {
-                chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+                http = new XMLHttpRequest();
+                http.timeout = 2000;
+                http.onreadystatechange = function() {
+                    if (this.readyState === 4) {
+                        if (this.status !== 200) {
+                            chrome.browserAction.setBadgeBackgroundColor({ color: [127, 127, 127, 255] });
+                        }
+                        else {
+                            chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+                        }
+                    }
+                };
+                http.open("HEAD", "https://google.com", true);
+                http.send();
             }
         }
     };
